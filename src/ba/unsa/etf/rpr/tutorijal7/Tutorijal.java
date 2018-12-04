58ff1c9e61037ab182dd8c3ed04b8da2f7952020
imaticevic1 <imaticevic1@etf.unsa.ba>
2018-12-04 12:15:32 +0100

    problem sa otvaranjem datoteke mjerenja.txt

--- src/ba/unsa/etf/rpr/tutorijal7/Tutorijal.java
+++ src/ba/unsa/etf/rpr/tutorijal7/Tutorijal.java
@@ -2,9 +2,34 @@ package ba.unsa.etf.rpr.tutorijal7;
 
 import java.beans.*;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Tutorijal {
+    public static ArrayList<Grad> ucitajGradove(){
+        ArrayList<Grad> gradovi = new ArrayList<Grad>();
+        Scanner ulaz;
+        try{
+            ulaz = new Scanner(new FileReader("mjerenja.txt"));
+        }
+        catch(FileNotFoundException e){
+            System.out.println("Datoteka mjerenja.txt se ne moze otvoriti ili ne postoji");
+            System.out.println("Greska: " + e);
+            return null;
+        }
+        /*try{
+            while(ulaz.hasNext()){
+            }
+            if(ulaz.next().getClass() == String.class){
+                System.out.println((String)ulaz.next());
+            }
+        }
+        catch (Exception e){
+            ulaz.close();
+        }*/
+        return null;
+    }
     public static void main(String[] args){
-
+        ucitajGradove();
     }
 }
